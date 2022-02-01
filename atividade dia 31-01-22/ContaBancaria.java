39ad4e2b7275d9bb4829516077dddeb56ac0e578
NadyneBarbieri <97990821+NadyneBarbieri@users.noreply.github.com>
2022-01-31 22:44:13 -0300

    Atividade do dia 31/12/22

---
+++ atividade dia 31-01-22/ContaBancaria.java
@@ -0,0 +1,21 @@
+package atividadeclasseeobjetos;
+
+public class ContaBancaria {
+	
+	//Atributos - variaveis
+	 String nome;
+	int agencia;
+	String conta;
+	int digito;
+	
+	
+	void extratoBancario() {
+		System.out.println("Puxando extrato bancario....");
+	}
+	void Saldo() {
+		System.out.println("seu saldo:");
+	}
+	
+	
+
+}
---
+++ atividade dia 31-01-22/TestacontaBancaria.java
@@ -0,0 +1,23 @@
+package atividadeclasseeobjetos;
+
+
+public class TestacontaBancaria {
+
+	public static void main(String[] args) {
+		
+		ContaBancaria c1 = new ContaBancaria();
+	
+		c1.nome = "Nadyne Mayara ";
+		c1.agencia = 111;
+		c1.conta = "93494790";
+		
+		
+		System.out.println(c1.nome);
+		System.out.println(c1.agencia);
+		System.out.println(c1.conta);
+		
+		
+
+	}
+
+}
