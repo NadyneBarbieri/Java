f99d65ed678166b23e92fcc5222ce8825165f410
Nadyne Barbieri <barbierinadyne@gmail.com>
2024-02-21 19:56:15 -0300

    mini projeto banco

---
+++ cursoUdemy/src/produtoEmEstoqueConstrutor/Product.java
@@ -0,0 +1,60 @@
+package produtoEmEstoqueConstrutor;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public Product() {}
+	public Product(String name, double price, int quantity){
+		//super();
+		this.name = name;
+		this.price = price;
+		this.quantity = quantity;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+
+	public double totalValueIntstock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity;
+	}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+	
+	public String toString() {
+		return name 
+				+ ", $ " 
+				+ String.format("%.2f", price)
+				+", "
+				+quantity
+				+" units, total: $ "
+				+String.format("%.2f", totalValueIntstock());
+	}
+	
+}
