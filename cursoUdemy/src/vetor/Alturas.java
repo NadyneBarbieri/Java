a5f4de930e5bae05ae4cf3d0b84d8b5452722c62
Nadyne Barbieri <barbierinadyne@gmail.com>
2024-02-22 22:22:18 -0300

    Correcao

---
+++ cursoUdemy/src/vetor/Alturas.java
@@ -0,0 +1,22 @@
+package vetor;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Alturas {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		double[] vect = new double [x];
+		for(int n=0; n < x; n++) {
+			vect[n]= sc.nextDouble();
+		}
+		
+		System.out.println(vect);
+		sc.close();
+	}
+
+}
