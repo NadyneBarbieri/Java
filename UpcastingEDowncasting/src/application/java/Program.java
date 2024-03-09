3185389365b1913ff171d7d88d0e0dafa9823a5f
Nadyne Barbieri <barbierinadyne@gmail.com>
2024-03-09 00:27:29 -0300

    Sobreposição

--- UpcastingEDowncasting/src/application/java/Program.java
+++ UpcastingEDowncasting/src/application/java/Program.java
@@ -9,28 +9,40 @@ public class Program {
 	public static void main(String[] args) {
 		Account acc = new Account(1001, "Alex", 0.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
-		
+
 		// UPCASTING
 		Account acc1 = bacc;
 		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
 		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
-		
+
 		// DOWNCASTING
-		BusinessAccount acc4 = (BusinessAccount)acc2;
+		BusinessAccount acc4 = (BusinessAccount) acc2;
 		acc4.loan(100.0);
+
+		Account acc6 = new Account(1003, "Mariah", 1000.0);
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
+
+		Account acc7 = new SavingsAccount(1004, "Marcia", 1000.0, 0.01);
+		acc7.withdraw(200.0);
+		System.out.println(acc7.getBalance());
+
+		Account acc8 = new BusinessAccount(1005, "Marcio", 1000.0, 500.0);
+		acc8.withdraw(200.0);
+		System.out.println(acc8.getBalance());
 		
 		// BusinessAccount acc5 = (BusinessAccount)acc3; nao da certo
-		if (acc3 instanceof BusinessAccount) {
-			BusinessAccount acc5 = (BusinessAccount)acc3;
-			acc5.loan(200.0);
-			System.out.println("Loan!");
-		}
-		
-		if (acc3 instanceof SavingsAccount) {
-			SavingsAccount acc5 = (SavingsAccount)acc3;
-			acc5.updateBalance();
-			System.out.println("Update!");
-		}
+//		if (acc3 instanceof BusinessAccount) {
+//			BusinessAccount acc5 = (BusinessAccount)acc3;
+//			acc5.loan(200.0);
+//			System.out.println("Loan!");
+//		}
+//		
+//		if (acc3 instanceof SavingsAccount) {
+//			SavingsAccount acc5 = (SavingsAccount)acc3;
+//			acc5.updateBalance();
+//			System.out.println("Update!");
+//		}
 	}
-	
+
 }
--- UpcastingEDowncasting/src/entities/BusinessAccount.java
+++ UpcastingEDowncasting/src/entities/BusinessAccount.java
@@ -20,10 +20,18 @@ public class BusinessAccount extends Account {
 	public void setLoanLimit(Double loanLimit) {
 		this.loanLimit = loanLimit;
 	}
-	
+	//emprestimo
 	public void loan(double amount) {
 		if (amount <= loanLimit) {
 			balance += amount - 10.0;
 		}
 	}
+	
+	@Override
+	public void withdraw(double amount) {
+		super.withdraw(amount);
+		balance -=2.0;
+	}
+	
+	
 }
