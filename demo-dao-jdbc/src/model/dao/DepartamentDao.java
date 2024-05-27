48f93b5916dda8213b0f13802e8dcdf43c048af2
Nadyne Barbieri <97990821+NadyneBarbieri@users.noreply.github.com>
2024-05-27 20:46:04 -0300

    SellerDaoJDBC and DaoFactory

--- demo-dao-jdbc/src/model/dao/DepartamentDao.java
+++ demo-dao-jdbc/src/model/dao/DepartamentDao.java
@@ -1,9 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
 import model.entities.Departament;
 
 public interface DepartamentDao {
 	
 	void inset(Departament obj);
+	void update(Departament obj);
+	void deletebyId(Integer obj);
+	Departament findbyId(Integer id);
+	List<Departament> findAll();
 
 }
